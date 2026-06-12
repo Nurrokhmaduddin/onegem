5d342d0be01bc06a0aa7207a1a41f15b1890873b
Nurrokhmaduddin <harunruddin54@gmail.com>
2026-06-12 21:22:54 +0700

    update db pass

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ define('DB_HOST',    '127.0.0.1');
 define('DB_PORT',    '3306');
 define('DB_NAME',    'erp_berlian');
 define('DB_USER',    'root');
-define('DB_PASS',    '');           // Laragon default: kosong
+define('DB_PASS',    'tmi2026');           // Laragon default: kosong
 define('DB_CHARSET', 'utf8mb4');
 
 /**
