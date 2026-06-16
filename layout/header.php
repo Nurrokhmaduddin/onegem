19e00e85a3b276140e7f29d1cfbbbdeac3b2b1b2
Nurrokhmaduddin <harunruddin54@gmail.com>
2026-06-16 14:23:15 +0700

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -204,12 +204,11 @@ if ($activePath !== '/') {
 
       <div class="nav-section-label">Penjualan</div>
 
-        <!-- <?php if (can('LEAD_VIEW')): ?> -->
+
           <a href="<?= url('sales/lead/list') ?>"
             class="nav-link <?= $activePath==='/sales/lead/list'?'active':'' ?>">
             <i class="bi bi-list-ul"></i>Lead
           </a>
-        <!-- <?php endif; ?> -->
 
 
       <?php if (can('LEAD_VIEW')): ?>
